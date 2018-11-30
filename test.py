4559d13c0b3f862c72b1d48828c9fbff6ce2d511
syusi <cs15044@s.inf.shizuoka.ac.jp>
2018-11-30 17:46:17 +0900

    アーティストごとにフォルダ分けをしてくれる

--- test.py
+++ test.py
@@ -1,46 +1,68 @@
 import sys
+import csv
 import urllib.request
 import urllib3
 import bs4
 import re
+import os
 
 baseUrl = "https://www.easy-youtube-mp3.com/download.php?"
+defaultDir = "./music"
+def getDirpath(name):
+    dirpath = defaultDir + "/" + name + "/"
+    if not os.path.isdir(dirpath):
+        os.mkdir(dirpath)
+    return dirpath
 
-url = sys.argv[1]
+filename = sys.argv[1]
 
-vPoint = re.search("v=",url)
-
-#print(url[vPoint.start():])
-
-url = baseUrl + url[vPoint.start():]
-
-
-print("開始します")
+#init
 urllib3.disable_warnings()
 http = urllib3.PoolManager()
+if not os.path.isdir(defaultDir):
+    os.makedirs(defaultDir)
 
-try:
-    res = http.request("GET",url)
-except:
-    print("url先が存在しません")
+csv_file = open(filename,"r",encoding="utf-8")
+
+reader = csv.reader(csv_file)
+for row in reader:
+    dirname = row[0]
+    savemane = row[1]
+    youtubeUrl = row[2]
+    vPoint = re.search("v=",youtubeUrl)
+    #print(url[vPoint.start():])
+
+    url = baseUrl + youtubeUrl[vPoint.start():]
 
 
-if res.status == 200:
-    print("OK\n")
-else:
-    print("url先が読み込めませんでした")
+    print("開始します")
+    urllib3.disable_warnings()
+    http = urllib3.PoolManager()
+    try:
+        res = http.request("GET",url)
+    except:
+        print("url先が存在しません")
+        continue
 
-soup = bs4.BeautifulSoup(res.data,"html.parser")
 
-print(str(soup.title) + "\n")
+    if res.status == 200:
+        print("OK\n")
+    else:
+        print("url先が読み込めませんでした")
+        continue
 
-a = soup.find_all("a",class_="btn btn-lg btn-success")
+    soup = bs4.BeautifulSoup(res.data,"html.parser")
 
-##print(a[0].get("href"))
+    print(str(soup.title) + "\n")
 
-url = a[0].get("href")
+    a = soup.find_all("a",class_="btn btn-lg btn-success")
 
-savemane = "test.mp3"
-urllib.request.urlretrieve(url,savemane)
+    ##print(a[0].get("href"))
 
-print("保存しました")
+    url = a[0].get("href")
+    path = getDirpath(dirname)
+    urllib.request.urlretrieve(url,path + savemane+".mp3")
+
+    print("保存しました")
+
+print("全て終了しました")
